848bf66f7bc41bc022a6ae72d0a88b2eba2c8aa1
Vyacheslav <brevnov_vs.nw@uraltexis.ru>
2022-08-31 04:08:04 +0300

    feat: added AsPointerAttribute #1

---
+++ Src/StackMemoryAttributes/Attributes/AsPointerAttribute.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace StackMemoryAttributes.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property)]
+    public class AsPointerAttribute : Attribute
+    {
+    }
+}
